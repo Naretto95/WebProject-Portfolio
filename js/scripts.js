42cadaf21ae98b7aecd9350e533bc5d8b054e02e
Lilian Naretto <narettolilian@hotmail.fr>
2020-02-21 19:57:00 +0100

    ajout des compétences

--- js/scripts.js
+++ js/scripts.js
@@ -28,5 +28,13 @@ jQuery(document).ready(function() {
 	
 });
 
+jQuery(document).ready(function(){
+	jQuery('.skillbar').each(function(){
+		jQuery(this).find('.skillbar-bar').animate({
+			width:jQuery(this).attr('data-percent')
+		},6000);
+	});
+});
+
 
 
